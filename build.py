5515fd7340920fdb5161f7b2c068ed60ce23c619
Grzegorz Halajko <gh@linuxpolska.pl>
2014-12-22 11:55:16 +0000

    add description and author

--- build.py
+++ build.py
@@ -1,6 +1,12 @@
-from pybuilder.core import use_plugin
+from pybuilder.core import use_plugin,Author,init
 
 use_plugin("python.core")
 use_plugin("python.distutils")
 
 default_task = "publish"
+description = """Linux Virtual Server (LVS) Top. Tool for LVS admins"""
+authors = [Author("Grzegorz Halajko", "ghalajko+lvstop@gmail.com")]
+
+@init
+def initialize(project):
+    project.version = "0.1.0"
